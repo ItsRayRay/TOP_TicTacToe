0b5bbb386184c2b9a83624058e3e1d6c3344d326
ItsRayRay <r.boerema21@gmail.com>
2022-06-13 21:36:26 +0200

    update script

--- script.js
+++ script.js
@@ -7,73 +7,65 @@ const playGame = (position) => {
         turn: true,
     }
     
-    let PlayerTwo = {
+    let playerTwo = {
         turn: false,
     } 
     
-    const gameBoard = {
-    board: [0,1,2,3,4,5,6,7,8],
-    emptyboard: [0,1,2,3,4,5,6,7,8],
+    let gameBoard = {
+    boardPlayerOne: [0,1,2,3,4,5,6,7,8],
+    boardPlayerTwo: [0,1,2,3,4,5,6,7,8],
 
-    winCombination : {
-        1: [0,1,2],
-        2: [3,4,5],
-        3: [6,7,8],
-        4: [0,3,6],
-        5: [1,4,7],
-        6: [2,5,8],
-        7: [0,4,8],
-        8: [2,4,6],     
-    },
     winCombinationsPlayerOne: {
-        1: [❌,❌,❌,3,4,5,6,7,8],
-        2: [0,1,2,❌,❌,❌,6,7,8],
-        3: [0,1,2,3,4,5,❌,❌,❌],
-        4: [❌,1,2,❌,4,5,❌,7,8],
-        5: [0,❌,2,3,❌,5,6,❌,8],
-        6: [0,1,❌,3,4,❌,6,7,❌],
-        7: [❌,1,2,3,❌,5,6,7,❌],
-        8: [0,1,❌,3,❌,5,❌,7,8],
-
-
+        1: ['❌','❌','❌',3,4,5,6,7,8],
+        2: [0,1,2,'❌','❌','❌',6,7,8],
+        3: [0,1,2,3,4,5,'❌','❌','❌'],
+        4: ['❌',1,2,'❌',4,5,'❌',7,8],
+        5: [0,'❌',2,3,'❌',5,6,'❌',8],
+        6: [0,1,'❌',3,4,'❌',6,7,'❌'],
+        7: ['❌',1,2,3,'❌',5,6,7,'❌'],
+        8: [0,1,'❌',3,'❌',5,'❌',7,8],
     },
     winCombinationsPlayerTwo: {
-        1: [⭕,⭕,⭕,3,4,5,6,7,8],
-        2: [0,1,2,⭕,⭕,⭕,6,7,8],
-        3: [0,1,2,3,4,5,⭕,⭕,⭕],
-        4: [⭕,1,2,⭕,4,5,⭕,7,8],
-        5: [0,⭕,2,3,⭕,5,6,⭕,8],
-        6: [0,1,⭕,3,4,⭕,6,7,⭕],
-        7: [⭕,1,2,3,⭕,5,6,7,⭕],
-        8: [0,1,⭕,3,⭕,5,⭕,7,8],
+        1: ['⭕','⭕','⭕',3,4,5,6,7,8],
+        2: [0,1,2,'⭕','⭕','⭕',6,7,8],
+        3: [0,1,2,3,4,5,'⭕','⭕','⭕'],
+        4: ['⭕',1,2,'⭕',4,5,'⭕',7,8],
+        5: [0,'⭕',2,3,'⭕',5,6,'⭕',8],
+        6: [0,1,'⭕',3,4,'⭕',6,7,'⭕'],
+        7: ['⭕',1,2,3,'⭕',5,6,7,'⭕'],
+        8: [0,1,'⭕',3,'⭕',5,'⭕',7,8],
 
     },
 
-
 }
-
 
 //if playerOne is true set x
 // if playerOne is false set o
 
-
 if (playerOne.turn) {
-    gameBoard.board[position] = 'x';
+    gameBoard.boardPlayerOne[position] = '❌';
     playerOne.turn = false;
     playerTwo.turn = true;
-
+} else if (playerTwo.turn) { { 
+    boardPlayerTwo.board[position] = '⭕';
+    playerOne.turn = true;
+    playerTwo.turn = false;
+}
  
 // if indexof 012 === x  set playerone to win
 // else if indexof 012 === 0 set player two to win
 // if index off all is x or o 
 
-
 }
 
 }
+
+playGame(1);
+
+
 
 //EventTarget.addEventListener("click", function() {
 
     // Do something cool
-    playGame(0);
+   
 //}, {once : true});
