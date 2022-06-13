effc2bdfbe933c5f0529c87e6ccc33a8ca82e7b6
ItsRayRay <r.boerema21@gmail.com>
2022-06-14 00:25:53 +0200

    get rid of DRY code

--- script.js
+++ script.js
@@ -1,98 +1,12 @@
-const upperLeftCorner = document.querySelector(".div1");
-const upperMid = document.querySelector(".div2");
-const upperRightCorner = document.querySelector(".div3");
-const midLeft = document.querySelector(".div4");
-const midMid = document.querySelector(".div5");
-const midRight = document.querySelector(".div6");
-const lowerLeftCorner = document.querySelector(".div7");
-const lowerMid = document.querySelector(".div8");
-const lowerRightCorner = document.querySelector(".div9");
 
-upperLeftCorner.addEventListener(
-  "click",
-  function () {
-    console.log("clicked upper left corner");
-    playGame(0);
-  },
-  { once: true }
-);
-
-upperMid.addEventListener(
-  "click",
-  function () {
-    console.log("clicked upper mid");
-    playGame(1);
-  },
-  { once: true }
-);
-
-upperRightCorner.addEventListener(
-  "click",
-  function () {
-    console.log("clicked upper right corner");
-    playGame(2);
-  },
-  { once: true }
-);
-
-midLeft.addEventListener(
-  "click",
-  function () {
-    console.log("clicked mid left");
-    playGame(3);
-  },
-  { once: true }
-);
-
-midMid.addEventListener(
-  "click",
-  function () {
-    console.log("clicked mid mid");
-    playGame(4);
-  },
-  { once: true }
-);
-
-midRight.addEventListener(
-  "click",
-  function () {
-    console.log("clicked mid right");
-    playGame(5);
-  },
-  { once: true }
-);
-
-lowerLeftCorner.addEventListener(
-  "click",
-  function () {
-    console.log("clicked lower left corner");
-    playGame(6);
-  },
-  { once: true }
-);
-
-lowerMid.addEventListener(
-  "click",
-  function () {
-    console.log("clicked lower mid");
-    playGame(7);
-  },
-  { once: true }
-);
-
-lowerRightCorner.addEventListener(
-  "click",
-  function () {
-    console.log("clicked lower right corner");
-    playGame(8);
-  },
-  { once: true }
-);
+function render(position) {
+  playGame(position)
+  console.log(position)
+}
 
 let gameBoard = {
     boardPlayerOne: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     boardPlayerTwo: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-
     winCombinationsPlayerOne: {
       1: ["❌", "❌", "❌", 3, 4, 5, 6, 7, 8],
       2: [0, 1, 2, "❌", "❌", "❌", 6, 7, 8],
@@ -114,7 +28,6 @@ let gameBoard = {
       8: [0, 1, "⭕", 3, "⭕", 5, "⭕", 7, 8],
     },
   };
-
   let playerOne = {
     turn: true,
 }
@@ -122,19 +35,15 @@ let gameBoard = {
 let playerTwo = {
     turn: false,
 }
-
 const playGame = (position) => {
-
-
 
   if (playerOne.turn) {
       gameBoard.boardPlayerOne[position] = '❌';
       playerOne.turn = false;
       playerTwo.turn = true;
       console.log(gameBoard.boardPlayerOne);
-        
-
-      
+        console.log(gameBoard.winCombinationsPlayerOne);
+     
   } else if (playerTwo.turn) { {
     gameBoard.boardPlayerTwo[position] = '⭕';
       playerOne.turn = true;
@@ -142,5 +51,4 @@ const playGame = (position) => {
       console.log(gameBoard.boardPlayerTwo);
   }
   
-
 }}
