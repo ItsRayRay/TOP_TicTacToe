f745b1b6628b79f78aa12666867c26a2ed91ad2d
ItsRayRay <r.boerema21@gmail.com>
2022-06-14 12:28:46 +0200

    render positions on screen when clicked

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-
 function render(position) {
   playGame(position)
-  console.log(position)
+}
+
+let playerOne = {
+  turn: true,
+}
+
+let playerTwo = {
+  turn: false,
 }
 
 let gameBoard = {
+    boardOnScreen: ["", "", "", "", "", "", "", "", ""],
     boardPlayerOne: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     boardPlayerTwo: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     winCombinationsPlayerOne: {
@@ -27,28 +34,34 @@ let gameBoard = {
       7: ["⭕", 1, 2, 3, "⭕", 5, 6, 7, "⭕"],
       8: [0, 1, "⭕", 3, "⭕", 5, "⭕", 7, 8],
     },
-  };
-  let playerOne = {
-    turn: true,
-}
 
-let playerTwo = {
-    turn: false,
-}
+  };
+  
+
+
 const playGame = (position) => {
 
   if (playerOne.turn) {
       gameBoard.boardPlayerOne[position] = '❌';
+      gameBoard.boardOnScreen[position] = '❌';
       playerOne.turn = false;
       playerTwo.turn = true;
       console.log(gameBoard.boardPlayerOne);
-        console.log(gameBoard.winCombinationsPlayerOne);
+      
+      for (let i = 0; i < gameBoard.boardOnScreen.length; i++) {
+        document.getElementById(i).textContent = gameBoard.boardOnScreen[i];
+       }
+      
      
   } else if (playerTwo.turn) { {
     gameBoard.boardPlayerTwo[position] = '⭕';
+    gameBoard.boardOnScreen[position] = '⭕';
       playerOne.turn = true;
       playerTwo.turn = false;
       console.log(gameBoard.boardPlayerTwo);
+      for (let i = 0; i < gameBoard.boardOnScreen.length; i++) {
+        document.getElementById(i).textContent = gameBoard.boardOnScreen[i];
+       }
   }
   
 }}
