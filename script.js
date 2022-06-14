c82be2597d8101de914c7fe8f2cad5adef633ee2
ItsRayRay <r.boerema21@gmail.com>
2022-06-14 12:39:53 +0200

    save gameboardwins in variables

--- script.js
+++ script.js
@@ -15,31 +15,34 @@ let gameBoard = {
     boardPlayerOne: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     boardPlayerTwo: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     winCombinationsPlayerOne: {
-      1: ["❌", "❌", "❌", 3, 4, 5, 6, 7, 8],
-      2: [0, 1, 2, "❌", "❌", "❌", 6, 7, 8],
-      3: [0, 1, 2, 3, 4, 5, "❌", "❌", "❌"],
-      4: ["❌", 1, 2, "❌", 4, 5, "❌", 7, 8],
-      5: [0, "❌", 2, 3, "❌", 5, 6, "❌", 8],
-      6: [0, 1, "❌", 3, 4, "❌", 6, 7, "❌"],
-      7: ["❌", 1, 2, 3, "❌", 5, 6, 7, "❌"],
-      8: [0, 1, "❌", 3, "❌", 5, "❌", 7, 8],
+      a: ["❌", "❌", "❌", 3, 4, 5, 6, 7, 8],
+      b: [0, 1, 2, "❌", "❌", "❌", 6, 7, 8],
+      c: [0, 1, 2, 3, 4, 5, "❌", "❌", "❌"],
+      d: ["❌", 1, 2, "❌", 4, 5, "❌", 7, 8],
+      e: [0, "❌", 2, 3, "❌", 5, 6, "❌", 8],
+      f: [0, 1, "❌", 3, 4, "❌", 6, 7, "❌"],
+      g: ["❌", 1, 2, 3, "❌", 5, 6, 7, "❌"],
+      h: [0, 1, "❌", 3, "❌", 5, "❌", 7, 8],
     },
     winCombinationsPlayerTwo: {
-      1: ["⭕", "⭕", "⭕", 3, 4, 5, 6, 7, 8],
-      2: [0, 1, 2, "⭕", "⭕", "⭕", 6, 7, 8],
-      3: [0, 1, 2, 3, 4, 5, "⭕", "⭕", "⭕"],
-      4: ["⭕", 1, 2, "⭕", 4, 5, "⭕", 7, 8],
-      5: [0, "⭕", 2, 3, "⭕", 5, 6, "⭕", 8],
-      6: [0, 1, "⭕", 3, 4, "⭕", 6, 7, "⭕"],
-      7: ["⭕", 1, 2, 3, "⭕", 5, 6, 7, "⭕"],
-      8: [0, 1, "⭕", 3, "⭕", 5, "⭕", 7, 8],
+      a: ["⭕", "⭕", "⭕", 3, 4, 5, 6, 7, 8],
+      b: [0, 1, 2, "⭕", "⭕", "⭕", 6, 7, 8],
+      c: [0, 1, 2, 3, 4, 5, "⭕", "⭕", "⭕"],
+      d: ["⭕", 1, 2, "⭕", 4, 5, "⭕", 7, 8],
+      e: [0, "⭕", 2, 3, "⭕", 5, 6, "⭕", 8],
+      f: [0, 1, "⭕", 3, 4, "⭕", 6, 7, "⭕"],
+      g: ["⭕", 1, 2, 3, "⭕", 5, 6, 7, "⭕"],
+      h: [0, 1, "⭕", 3, "⭕", 5, "⭕", 7, 8],
     },
 
   };
-  
 
+  let winOne = gameBoard.winCombinationsPlayerOne
+  let winTwo = gameBoard.winCombinationsPlayerTwo
 
 const playGame = (position) => {
+
+  
 
   if (playerOne.turn) {
       gameBoard.boardPlayerOne[position] = '❌';
@@ -47,9 +50,12 @@ const playGame = (position) => {
       playerOne.turn = false;
       playerTwo.turn = true;
       console.log(gameBoard.boardPlayerOne);
+      console.log(winOne.a)
       
       for (let i = 0; i < gameBoard.boardOnScreen.length; i++) {
         document.getElementById(i).textContent = gameBoard.boardOnScreen[i];
+
+
        }
       
      
@@ -65,3 +71,5 @@ const playGame = (position) => {
   }
   
 }}
+
+
